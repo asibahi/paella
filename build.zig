e5d8ea17056548858d0c3cec785f0d7c3c0468e3
Abdul Rahman Sibahi <a.sibahi@gmail.com>
2025-05-11 20:27:17 +0300

    add the intended testing apparatus to build.zig

--- build.zig
+++ build.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
 
@@ -25,20 +25,33 @@ pub fn build(b: *std.Build) void {
         const run_step = b.step("run", "Run the app");
 
         const run_cmd = b.addRunArtifact(exe);
-        run_cmd.step.dependOn(b.getInstallStep());
-        if (b.args) |args| { // pass aruments into `zig build run --`
+        if (b.args) |args| // pass aruments into `zig build run --`
             run_cmd.addArgs(args);
-        }
 
+        run_cmd.step.dependOn(b.getInstallStep());
         run_step.dependOn(&run_cmd.step);
     }
 
     { // `zig build test` command
         const test_step = b.step("test", "Run tests");
 
-        const exe_unit_tests = b.addTest(.{ .root_module = exe_mod });
-        const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+        // subshells. how do they work.
+        const inner_command = try std.mem.join(b.allocator, " ", &.{
+            "../writing-a-c-compiler-tests/test_compiler",
+            b.pathJoin(&.{ b.exe_dir, "paella" }),
+            try std.mem.join(
+                b.allocator,
+                " ",
+                b.args orelse &.{""},
+            ),
+        });
 
-        test_step.dependOn(&run_exe_unit_tests.step);
+        // does this work like i think it does?
+        const test_command = b.addSystemCommand(
+            &.{ "arch", "-x86_64", "zsh", "-c", inner_command },
+        );
+
+        test_command.step.dependOn(b.getInstallStep());
+        test_step.dependOn(&test_command.step);
     }
 }
